bfe80404856e10c056d5ebd0f9011a50f2ded8c5
Amazingandyyy <amazingandyyy@gmail.com>
2016-06-15 00:15:44 -0700

    add search bar
    add search bar

--- client/js/controllers/dashboardApplicationsCtrl.js
+++ client/js/controllers/dashboardApplicationsCtrl.js
@@ -8,9 +8,13 @@ angular
 function dashboardApplicationsCtrl($scope, Application) {
     console.log("dashboardApplicationsCtrl loaded");
     Application.getAllApplications().then(res => {
-           console.log('res: ', res.data);
-           $scope.applications = res.data
-       }, err => {
-           console.log('err when getting all applications: ', err);
-       })
+        // console.log('res: ', res.data)
+        $scope.applications = res.data
+        $scope.createTime = (time) => {
+            return moment(time).format('LT')
+        }
+    }, err => {
+        console.log('err when getting all applications: ', err);
+    })
+
 }
---
+++ client/js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+$(document).ready(init);
+
+function init() {
+    sticky()
+}
+
+
+function sticky() {
+    $(".sticky").sticky({
+        topSpacing: 100
+    })
+}
