b10aaabab14e9454272dedee604bce864f38a74b
Amazingandyyy <amazingandyyy@gmail.com>
2016-06-14 21:24:38 -0700

    merge and beautify the models
    merge and beautify the models

--- models/user.js
+++ models/user.js
@@ -7,67 +7,108 @@ var bcrypt = require("bcryptjs");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 var userSchema = new mongoose.Schema({
-    email : { type : String, unique : true},
-    password : { type : String},
-    github : { type: String },
-    google : { type : String },
-    biography : { type : String },
-    linksToProfiles : [{ type: String }],
-    name : { type : String },
-    photo : { type : String },
-    friends : [{ type : mongoose.Schema.Types.ObjectId, ref : "User" }],
-    applications : [{ type : mongoose.Schema.Types.ObjectId, ref : "Application" }]
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    github: {
+        type: String
+    },
+    google: {
+        type: String
+    },
+    biography: {
+        type: String
+    },
+    linksToProfiles: [{
+        type: String
+    }],
+    name: {
+        type: String
+    },
+    photo: {
+        type: String
+    },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    applications: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Application"
+    }]
 });
 
-userSchema.statics.register = function (userObject, callback) {
-    User.findOne({ email : userObject.email}, function (error, userData) {
-        if (error || userData) return callback(error || { error : "Email Not Available" });
+userSchema.statics.register = function(userObject, callback) {
+    User.findOne({
+        email: userObject.email
+    }, function(error, userData) {
+        if (error || userData) return callback(error || {
+            error: "Email Not Available"
+        });
 
-        bcrypt.hash(userObject.password, 12, function (error, hash) {
+        bcrypt.hash(userObject.password, 12, function(error, hash) {
             if (error) return callback(error);
 
             var user = new User({
-                email : userObject.email,
-                password : hash
+                email: userObject.email,
+                password: hash
             });
             user.save(callback);
         });
     });
 };
 
-userSchema.methods.generateToken = function () {
+userSchema.methods.generateToken = function() {
     var token = jwt.sign({
-        _id : this._id
+        _id: this._id
     }, JWT_SECRET);
     return token;
 };
 
-userSchema.statics.isLoggedIn = function (request, response, next) {
+userSchema.statics.isLoggedIn = function(request, response, next) {
 
     if (!request.header("Authorization")) {
-        return response.status(401).send({ message : "PLease make sure request has an Authorization Header"});
+        return response.status(401).send({
+            message: "PLease make sure request has an Authorization Header"
+        });
     }
 
     var token = request.header("Authorization").split(" ")[1];
 
-    jwt.verify(token, JWT_SECRET, function (error, payload) {
-        if (error) return response.status(401).send({error : "Must be authenticated"});
+    jwt.verify(token, JWT_SECRET, function(error, payload) {
+        if (error) return response.status(401).send({
+            error: "Must be authenticated"
+        });
 
-        User.findById(payload._id, function (error, userData) {
-            if (error || !userData) return response.clearCookie("accessToken").status(400).send(error || {error : "User not found"});
+        User.findById(payload._id, function(error, userData) {
+            if (error || !userData) return response.clearCookie("accessToken").status(400).send(error || {
+                error: "User not found"
+            });
             request.user = userData;
             next();
-        }).select({password : false})
+        }).select({
+            password: false
+        })
     })
 
 };
 
-userSchema.statics.authenticate = function (loginData, callback) {
-    User.findOne({email : loginData.email}, function (error, userData) {
-        if (error || !userData) return callback(error || {error : "Login Failed. Email or Password is Incorrect"});
+userSchema.statics.authenticate = function(loginData, callback) {
+    User.findOne({
+        email: loginData.email
+    }, function(error, userData) {
+        if (error || !userData) return callback(error || {
+            error: "Login Failed. Email or Password is Incorrect"
+        });
 
-        bcrypt.compare(loginData.password, userData.password, function (error, isGood) {
-            if (error || !isGood) return callback(error || { error : "Login Failed. Email or Password Incorrect"});
+        bcrypt.compare(loginData.password, userData.password, function(error, isGood) {
+            if (error || !isGood) return callback(error || {
+                error: "Login Failed. Email or Password Incorrect"
+            });
 
             var token = userData.generateToken();
 
@@ -82,5 +123,5 @@ userSchema.statics.addApplication = (user, application, cb) => {
 
 var User = mongoose.model("User", userSchema);
 
-module.exports = User;
->>>>>>> tsinat:models/user.js
+module.exports = User; >>>
+>>> > tsinat: models / user.js
