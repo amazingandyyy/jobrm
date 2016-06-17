909432548f744387b9c2c2a88c832ee8a06d65c5
march-dave <ysangy@gmail.com>
2016-06-17 10:30:38 -0700

    up to date

--- routes/twilio.js
+++ routes/twilio.js
@@ -4,23 +4,36 @@ var express = require("express");
 var router = express.Router();
 var twilio = require('twilio');
 
+var CronJob = require('cron').CronJob;
+
 router.post("/", (req, res) => {
 
    const TWILIO_ACCOUNTSID = process.env.TWILIO_ACCOUNTSID;
    const TWILIO_AUTHTOKEN = process.env.TWILIO_AUTHTOKEN;
    const TWILIO_FROM = process.env.TWILIO_FROM;
 
-   var client = require('twilio')(TWILIO_ACCOUNTSID, TWILIO_AUTHTOKEN);
-   var twilioto = req.body.twilioto;
-   var twiliobody = req.body.twiliobody;
+   // var client = require('twilio')(TWILIO_ACCOUNTSID, TWILIO_AUTHTOKEN);
+   // var twilioto = req.body.twilioto;
+   // var twiliobody = req.body.twiliobody;
 
-   client.messages.create({
-       to: twilioto,
-       from: TWILIO_FROM,
-       body: twiliobody
-   },  (err, message) => {
-      res.status(err ? 400 : 200).send(err);
-   });
+   // client.messages.create({
+   //     to: twilioto,
+   //     from: TWILIO_FROM,
+   //     body: twiliobody
+   // },  (err, message) => {
+      // res.status(err ? 400 : 200).send(err);
+   // });
+
+   // new CronJob('* * * * * *', function() {
+   /*
+      Runs everyday 11:30: AM Monday through Sunday
+   */
+   new CronJob('00 30 11 * * 0-6', function() {
+      console.log('You will see this message every second', Date());
+   }, true, true, 'America/Los_Angeles');
+
+   res.status(200).send(Date())
+
 });
 
 module.exports = router;
