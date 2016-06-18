2db9d9af8121a1d4ff68af381891a10f8fedfd20
Amazingandyyy <amazingandyyy@gmail.com>
2016-06-18 01:17:20 -0700

    I am designing the map(milestones) page
    I am designing the map(milestones) page

--- client/js/controllers/dashboardMapCtrl.js
+++ client/js/controllers/dashboardMapCtrl.js
@@ -16,4 +16,16 @@ function dashboardMapCtrl($stateParams, $scope, Application, $timeout, $state) {
             console.log('err when getting all applications: ', err);
         })
     }
+
+    $scope.createTime = (time) => {
+        return moment(time).calendar(null, {
+            sameDay: 'h:mm:ss a, [Today]',
+            nextDay: 'h:mm:ss a, [Tomorrow]',
+            nextWeek: 'dddd',
+            lastDay: 'h:mm:ss a, [Yesterday]',
+            lastWeek: 'h:mm:ss a, ddd. MMMM Do YYYY',
+            sameElse: 'MM/DD/YY'
+        });
+        // hh:mm:ss,
+    }
 }
