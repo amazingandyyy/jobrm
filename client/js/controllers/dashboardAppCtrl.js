6626a2a93ff48d2d6db76eb3cb80ad3875b344cc
Amazingandyyy <amazingandyyy@gmail.com>
2016-06-19 15:26:34 -0700

    start to update the form
    start to update the form

--- client/js/controllers/dashboardAppCtrl.js
+++ client/js/controllers/dashboardAppCtrl.js
@@ -7,20 +7,6 @@ angular
 function dashboardAppCtrl($stateParams, $scope, Application, $timeout, $state, store, $location) {
     console.log("dashboardAppCtrl loaded");
 
-    $scope.applicationDateDefault = moment().format('YYYY-MM-DD');
-    $scope.newApplicationSubmitted = () => {
-        console.log('$scope.newApplication: ', $scope.newApplication)
-        Application.createOneApplication($scope.newApplication, $scope.currentUser._id).then(res => {
-            console.log('newApplication res: ', res.data)
-            $state.go('application', {
-                applicationId: res.data.newApplication._id
-            })
-            $scope.applications.unshift(res.data.newApplication)
-        }, err => {
-            console.log('err when getting all applications: ', err);
-        })
-    }
-
     if ($stateParams.applicationId) {
         console.log('applicationId: ', $stateParams.applicationId);
         Application.getOneApplication($stateParams.applicationId).then(res => {
