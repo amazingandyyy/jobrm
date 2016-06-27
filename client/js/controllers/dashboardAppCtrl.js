4af1f6332baffcf05b467c01a0237d74fdc050d8
Amazingandyyy <amazingandyyy@gmail.com>
2016-06-26 22:09:12 -0700

    fixing date and time of milestone
    fixing date and time of milestone

--- client/js/controllers/dashboardAppCtrl.js
+++ client/js/controllers/dashboardAppCtrl.js
@@ -11,9 +11,9 @@ function dashboardAppCtrl($stateParams, $scope, Application, GoogleCalendarServi
         Application.getOneApplication($stateParams.applicationId).then(res => {
             console.log('Narrative: ', res.data);
             $scope.application = res.data;
-            if(res.data.length < 1){
+            if (res.data.length < 1) {
                 $state.go('dashboard');
-            }else{
+            } else {
                 $scope.applicationDetail = angular.copy(res.data);
                 let applicationDateDate = res.data.applicationDate.split("T")[0];
                 let expectedInitialResponseDate = res.data.expectedInitialResponse.split("T")[0];
@@ -21,6 +21,21 @@ function dashboardAppCtrl($stateParams, $scope, Application, GoogleCalendarServi
                 $scope.applicationDetail.expectedInitialResponse = new Date(expectedInitialResponseDate);
                 $scope.mileStones = res.data.milestones;
                 console.log('MileStones', $scope.mileStones);
+                $scope.stoneDateTime = (date, time) => {
+                    let timeDisplay = time.split("T")[1].split(':').slice(0, 2).join(':');;
+                    let dateDisplay = moment(date).calendar(null, {
+                        sameDay: 'h:mm a, [Today]',
+                        nextDay: 'h:mm a, [Tomorrow]',
+                        nextWeek: 'dddd',
+                        lastDay: 'h:mm:ss a, [Yesterday]',
+                        lastWeek: 'h:mm:ss a, ddd. MMMM Do YYYY',
+                        sameElse: 'MM/DD/YY'
+                    })
+                    console.log('timeDisplay: ', timeDisplay);
+                    return `${timeDisplay}, ${dateDisplay}`;
+                    // console.log();
+
+                }
             }
         }, err => {
             console.log('err when getting all application and all milstones: ', err);
@@ -109,9 +124,11 @@ function dashboardAppCtrl($stateParams, $scope, Application, GoogleCalendarServi
         console.log('dbStone: ', $scope.dbStone);
         let applicationId = $stateParams.applicationId;
         let toSend = angular.copy($scope.dbStone);
-        // toSend.date = moment(toSend.date).format("YYYY MM DD");
-        toSend.date = moment(toSend.date).format("YYYY MM DD").replace(/\s/gi, "-");
-        console.log("To Send: ", toSend)
+        console.log('toSend: ', toSend);
+        // toSend.date = moment(toSend.date).format("YYYY MM DD").replace(/\s/gi, "-");
+        // console.log("To Send: ", toSend)
+        // console.log("To Send time: ", toSend.time.toISOString().split("T")[1].split(':'))
+        // console.log("To Send time: ", toSend.time)
         Milestone.createOneMilestone(toSend, applicationId, store.get("googleAPIAccess"))
             .then(res => {
                 $scope.mileStones = res.data.dbSavedApplication.milestones;
@@ -157,8 +174,6 @@ function dashboardAppCtrl($stateParams, $scope, Application, GoogleCalendarServi
         console.log('applicationDetailUpdated: ', $scope.applicationDetail);
         Application.updateApplication($scope.applicationDetail, $stateParams.applicationId).then(res => {
             console.log('applicationDetailUpdated res: ', res.data);
-            //$window.location.reload();
-
         }, err => {
             console.log('err when applicationDetailUpdated: ', err);
         })
@@ -180,6 +195,9 @@ function dashboardAppCtrl($stateParams, $scope, Application, GoogleCalendarServi
             console.log('stone before setting: ', res.data);
             $scope.dbStoneUpdate = angular.copy(res.data);
             $scope.dbStoneUpdate._id = res.data._id;
+            let accurateDate = moment(res.data.date)
+            console.log('accurateDate: ' ,accurateDate);
+            $scope.dbStoneUpdate.date = new Date(accurateDate);
             $scope.isTheOne = (stoneId) => {
 
                 if (stoneId == res.data._id) {
@@ -200,7 +218,10 @@ function dashboardAppCtrl($stateParams, $scope, Application, GoogleCalendarServi
         console.log('dbStoneUpdate triggerred');
         Milestone.updateMilestone(dbStoneUpdate, stoneId).then(res => {
             console.log('stone updated, res: ', res.data);
-            $window.location.reload();
+            // $window.location.reload();
+            $state.go($state.current, {}, {
+                reload: true
+            });
         }, err => {
             console.log('err when updating one stone: ', err);
         })
@@ -212,6 +233,9 @@ function dashboardAppCtrl($stateParams, $scope, Application, GoogleCalendarServi
         Milestone.deleteMilestone(applicationId, milestoneId).then(res => {
             console.log('stone delete, res: ', res.data);
             // $window.location.reload();
+            $state.go($state.current, {}, {
+                reload: true
+            });
             GoogleCalendarServices.deleteCalendaredEvent(store.get("currentUserMId"), milestoneId, store.get("googleAPIAccess"))
                 .then((response) => {
                     console.log("Response: ", response);
